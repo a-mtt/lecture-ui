9ad6a9a2566bad36cde3920dd8d2701ef0c71679
Antoine Matta <antoine.matta@hotmail.com>
2023-06-10 10:18:07 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+
+import numpy as np
+import pandas as pd
+
+st.secrets['secret1']
+st.secrets['new_section']['secret2']
+
+direction = st.radio('Select a direction', ('top', 'right', 'bottom', 'left'))
+st.write(direction)
+if direction == "top":
+    st.write("^")
+
+st.markdown("""# This is a header
+## This is a sub header
+This is text""")
+
+df = pd.DataFrame({
+    'first column': list(range(1, 11)),
+    'second column': np.arange(10, 101, 10)
+})
+
+# this slider allows the user to select a number of lines
+# to display in the dataframe
+# the selected value is returned by st.slider
+line_count = st.slider('Select a line count', 1, 10, 3)
+
+# and used to select the displayed lines
+head_df = df.head(line_count)
+
+head_df
